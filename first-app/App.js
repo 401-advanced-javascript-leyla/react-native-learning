bb2a5d75fd392dfa4dc8753f276284cc157966c0
leyla li <leylali@qq.com>
2019-10-30 14:54:53 -0700

    built a textInput with button

--- first-app/App.js
+++ first-app/App.js
@@ -1,21 +1,17 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  const [outpuText, setOutputText] = useState('Open up App.js to start working on your app!');
   return (
-    <View style={styles.container}>
-      <Text>{outpuText}</Text>
-      <Button title='Change Text' onPress={() => setOutputText('The text is updated!')} />
+    <View style={{padding: 50}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+        <TextInput placeholder='Please enter your goal here' style={{width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}} />
+        <Button title='Add' />
+      </View>
+      
+      <View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

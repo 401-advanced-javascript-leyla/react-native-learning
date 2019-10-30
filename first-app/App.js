0b8c0985f3f004f0aa2bcc2b4bebaa726a6b242f
leyla li <leylali@qq.com>
2019-10-30 15:23:17 -0700

    added funtionalities to update list adding

--- first-app/App.js
+++ first-app/App.js
@@ -2,14 +2,27 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [goals, setGoals] = useState([]); 
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  }
+
+  const addGoalHnadler = () => {
+    setGoals(currentGoals => [...currentGoals, enteredGoal]);
+  }
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder='Please enter your goal here' style={styles.text} />
-        <Button title='Add' />
+        <TextInput placeholder='Please enter your goal here' style={styles.text} onChangeText={goalInputHandler} value={enteredGoal} />
+        <Button title='Add' onPress={addGoalHnadler} />
       </View>
 
       <View>
+        {goals.map((goal) => <Text key={goal}>{goal}</Text>
+        )}
       </View>
     </View>
   );

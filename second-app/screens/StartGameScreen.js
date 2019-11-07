1e7b62027a5a0c1498f5b5a930864d5273f942c3
leyla li <leylali@qq.com>
2019-11-07 14:27:35 -0800

    style input box and make it a seperate component, set props to control type of text that could be entered

--- second-app/screens/StartGameScreen.js
+++ second-app/screens/StartGameScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
 import Card from '../components/Card';
 import Colors from '../constants/colors';
+import Input from '../components/Input';
 
 const StartGameScreen = props => {
     return(
@@ -10,7 +11,7 @@ const StartGameScreen = props => {
             <Text style={styles.title}>'Start the Game!'</Text>    
             <Card style={styles.inputContainer}>
                 <Text>Select a number</Text>
-                <TextInput />
+                <Input style={styles.input} blurOnSubmit autoCapitalize='none' autoCorrect={false} keyboardType='number-pad' maxLenght={2} />
                 <View style={styles.buttonContainer}>
                     <View style={styles.button}><Button title='Reset' onPress={()=>{}} color={Colors.accent} /></View>
                     <View style={styles.button}><Button title='Confirm' onPress={()=>{}} color={Colors.primary} /></View>
@@ -43,7 +44,10 @@ const styles = StyleSheet.create({
     },
     button:{
         width: 100,
-
+    },
+    input: {
+        width: 100,
+        borderBottomColor: 'black'
     }
 });
 

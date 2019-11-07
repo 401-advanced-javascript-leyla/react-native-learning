449d41c7c0112693c50354e669e564528f047350
leyla li <leylali@qq.com>
2019-11-07 14:54:18 -0800

    show entered number

--- second-app/screens/StartGameScreen.js
+++ second-app/screens/StartGameScreen.js
@@ -4,16 +4,37 @@ import { View, Text, StyleSheet, Button, TouchableWithoutFeedback, Keyboard } fr
 import Card from '../components/Card';
 import Colors from '../constants/colors';
 import Input from '../components/Input';
+import { parse } from 'qs';
+import { selectAssetSource } from 'expo-asset/build/AssetSources';
 
 const StartGameScreen = props => {
     const [enteredValue, setEnteredValue] = useState('');
+    const [confirmed, setConfirmed] = useState(false);
+    const [selectedNumber, setselectedNumber] = useState('');
+
     const numberInputHandler = inputText => {
         setEnteredValue(inputText.replace(/[^0-9]/g, ''));
     };
 
     const resetInputHandler = () => {
         setEnteredValue('');
+        setConfirmed(false);
     };
+
+    const confirmInputHandler = () => {
+        const chosenNumber = parseInt(enteredValue);
+        if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99){
+            return;
+        }
+        setConfirmed(true);
+        setselectedNumber(parseInt(enteredValue));
+        setEnteredValue('');
+    };
+
+    let confirmedOutput;
+    if (confirmed) {
+        confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>
+    }
 
     return(
         <TouchableWithoutFeedback onPress={()=>{
@@ -23,12 +44,13 @@ const StartGameScreen = props => {
                 <Text style={styles.title}>'Start the Game!'</Text>    
                 <Card style={styles.inputContainer}>
                     <Text>Select a number</Text>
-                    <Input style={styles.input} blurOnSubmit autoCapitalize='none' autoCorrect={false} keyboardType='number-pad' maxLenth={2} onChangeText={numberInputHandler} />
+                    <Input value={enteredValue} style={styles.input} blurOnSubmit autoCapitalize='none' autoCorrect={false} keyboardType='number-pad' maxLenth={2} onChangeText={numberInputHandler} />
                     <View style={styles.buttonContainer}>
                         <View style={styles.button}><Button title='Reset' onPress={resetInputHandler} color={Colors.accent} /></View>
-                        <View style={styles.button}><Button title='Confirm' onPress={()=>{}} color={Colors.primary} /></View>
+                        <View style={styles.button}><Button title='Confirm' onPress={confirmInputHandler} color={Colors.primary} /></View>
                     </View>
-                </Card>           
+                </Card>
+                {confirmedOutput}           
             </View> 
         </TouchableWithoutFeedback>
    

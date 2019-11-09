6f2e3dc4043b9f31e3e2ebc21bff831880a7cc78
leyla li <leylali@qq.com>
2019-11-08 16:22:42 -0800

    style gameover page

--- second-app/screens/GamOverScreen.js
+++ second-app/screens/GamOverScreen.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
+import Colors from '../constants/colors';
+import Card from '../components/Card';
 
 const GameOverScreen = props => {
     return(
-    <View style={styles.screen}>
-        <Text> You got the right number! </Text>
-        <Text> Number of Guesses: {props.roundsNum} </Text>
-        <Text>Number was: {props.userNumber}</Text>
-        <Button title='New Game' onPress={props.onRestart} />
-    </View>
+    <Card>
+        <Text style={styles.text}> You got the right number! </Text>
+        <Text style={styles.text}> Number of Guesses: {props.roundsNum} </Text>
+        <Text style={styles.text}>Number was: {props.userNumber}</Text>
+        <View style={styles.button}><Button title='New Game' onPress={props.onRestart} /></View>
+    </Card>
     );
 };
 
@@ -17,6 +19,16 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    button: {
+        borderColor: Colors.primary,
+        borderRadius: 10,
+        borderWidth: 2
+    },
+    text: {
+        padding: 10,
+        color: Colors.primary,
+        fontSize: 22
     }
 });
 
